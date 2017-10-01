4f73ca5f0a26dbba350636e0e45c30dd83f38097
timejp <tokijh@naver.com>
2017-10-02 03:18:38 +0900

    Initial Commit

---
+++ MyOpenGLExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOpenGLExample
+//
+//  Created by 윤중현 on 2017. 10. 2..
+//  Copyright © 2017년 윤중현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
